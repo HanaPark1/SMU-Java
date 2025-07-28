bc65563bb4c6ff3d5048216ba9c5debcaf0cfdd6
HanaPark1 <ydjxlst@gmail.com>
2025-07-28 09:23:22 +0900

    study: 객체 생성, 선언 학습

---
+++ Ja0725_02/src/C0725_01.java
@@ -0,0 +1,21 @@
+
+public class C0725_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 변수 사용
+		int a = 10;
+		String str2 = "abc"; // 대입연산자 사용
+		// 참조형변수, 참조변수
+		String str = new String("abc");
+		
+		// Tv class 사용법
+		Tv t = new Tv();
+		
+		// 객체 선언
+		Stuscore s = new Stuscore();
+		
+
+	}
+
+}
---
+++ Ja0725_02/src/Tv.java
@@ -0,0 +1,17 @@
+
+public class Tv {
+	String color; // 변수
+	boolean power; // 변수
+ 	int channel; // 변수
+ 	
+ 	void power() {
+ 		power = !power;
+ 	}
+ 	
+ 	void channelUp() {
+ 		channel++;
+ 	}
+ 	void channelDown() {
+ 		channel--;
+ 	}
+}
