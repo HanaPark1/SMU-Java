4e51d0be73c98bf5824ffd89e49b710bf33178bc
HanaPark1 <ydjxlst@gmail.com>
2025-07-29 18:30:41 +0900

    study: 객체 생성

---
+++ Ja0729/src/C0729_08.java
@@ -0,0 +1,22 @@
+
+public class C0729_08 {
+
+	public static void main(String[] args) {
+		
+		Stuscore s1 = new Stuscore();
+		s1.name = "홍길동";
+		s1.kor = 100;
+		s1.eng = 100;
+		s1.math = 99;
+		
+//		Car2 c = new Car2();
+//		
+//		c.color = "white";
+//		c.gearType = "auto";
+//		c.door = 5;
+//		
+//		Car2 c2 = new Car2("pink", "stick", 5);
+
+	}
+
+}
