94c959b4e0bbc9bc1f511474f9858bbf24bf8194
HanaPark1 <ydjxlst@gmail.com>
2025-07-31 16:43:18 +0900

    Study: 인터페이스

---
+++ Ja0731/src/C0731_05.java
@@ -0,0 +1,22 @@
+
+public class C0731_05 {
+
+	public static void main(String[] args) {
+//		MImpl mImpl = new MImpl();
+//		mImpl.all_print();
+//		mImpl.add();
+//		mImpl.update();
+//		mImpl.delete();
+//		
+//		MImpl2 mImpl2 = new MImpl2();
+//		mImpl2.all_print();
+//		mImpl2.add();
+//		mImpl2.update();
+//		mImpl2.delete();
+		
+		MService mService = new MImpl2();
+		mService.all_print();
+
+	}
+
+}
---
+++ Ja0731/src/MService.java
@@ -0,0 +1,7 @@
+
+public interface MService {
+	void all_print();
+	void add();
+	void update();
+	void delete();
+}
