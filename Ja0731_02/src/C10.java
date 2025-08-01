054aeb7444b759e1e0ea40ccef5acd88073ece02
HanaPark1 <ydjxlst@gmail.com>
2025-08-01 17:52:10 +0900

    practice: 학생성적프로그램삭제구현 (ArrayList 활용)

--- Ja0731_02/src/C10.java
+++ Ja0731_02/src/C10.java
@@ -5,54 +5,52 @@ public class C10 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		StuDeck studect = new StuDeck();
-		ArrayList list = new ArrayList();
-		String name;
-		int kor,eng,math,total,rank;
-		int count = 0;
-		double avg;
-		
-		Stuscore stu = null;
-		
-		String[] title = {
-				"번호", "이름", "국어", "영어", "수학", "합계", "평균", "등수"
-		};
+		ArrayList<Stuscore> list = new ArrayList<Stuscore>();
+		StuDeck stuDeck = new StuDeck();
 		
 		while(true) {
-			int choice = studect.menu_print();
+			int choice = stuDeck.menu_print();
 			
 			switch(choice) {
 			case 1:
-				System.out.println("[ 학생성적입력 ]");
-				System.out.printf("%d번째 학생의 이름을 입력하세요>>>>>.\n",count+1);
-				name = scan.next();
-				System.out.println("국어 성적을 입력하세요>>>>>.");
-				kor = scan.nextInt();
-				System.out.println("영어 성적을 입력하세요>>>>>.");
-				eng= scan.nextInt();
-				System.out.println("수학 성적을 입력하세요>>>>>.");
-				math = scan.nextInt();
-				
-				list.add(new  Stuscore(name,kor,eng,math));
-				count++;
-				
-				System.out.printf("%s 학생 성적을 등록하였습니다.\n", name);
-				System.out.println();
-					
+				stuDeck.insert_stu(list);
 				break;
 				
 			case 2:
-				System.out.println("[ 학생성적출력 ]");
-				System.out.println("-----------------------------------------------------------");
-				System.out.printf("%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
-						title[0],title[1],title[2],title[3],title[4],title[5],title[6],title[7]);
-				for(int i=0; i<list.size(); i++) {
-					stu = (Stuscore) list.get(i);
-					System.out.printf("%d\t%s\t%d\t%d\t%d\t%d\t%.2f\t%d\n",
-							stu.getNo(),stu.getName(), stu.getKor(), stu.getEng(), stu.getMath(), stu.getTotal(), stu.getAvg(), stu.getRank());
-				}
-				System.out.println();
+				stuDeck.print_stu(list);
 				break;
+				
+			case 4:
+				System.out.println();
+				System.out.println("[ 학생성적삭제 ]");
+				System.out.println("삭제할 이름을 입력하세요.");
+				String name = scan.next();
+				int temp = 0;
+				for(int i=0; i<list.size(); i++) {
+					if(list.get(i).getName().contains(name)) {
+						temp = 1;
+						System.out.println("[ 학생검색이 되었습니다 ]");
+						System.out.printf("[ %s 학생삭제 ]\n",list.get(i).getName());
+						System.out.println("1. 삭제");
+						System.out.println("2. 취소");
+						choice = scan.nextInt();
+						switch(choice) {
+						case 1: 
+							System.out.printf(" %s 학생을 삭제했습니다. \n", list.get(i).getName());
+							list.remove(i);
+							break;
+						case 2:
+							System.out.println("삭제가 취소되었습니다.");
+							break;
+						}
+					}
+					
+				}
+				if (temp == 0) {
+					System.out.println(" [학생검색] ");
+					System.out.println("찾고자 하는 학생이 없습니다. 다시 입력하세요!");
+					System.out.println();
+				}
 			}
 		}
 		
