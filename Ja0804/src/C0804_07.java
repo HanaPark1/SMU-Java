a2e20e384a7727b2df10d628f649d945429840c6
HanaPark1 <ydjxlst@gmail.com>
2025-08-04 18:28:09 +0900

    study: I/OStream

---
+++ Ja0804/src/C0804_07.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class C0804_07 {
+
+	public static void main(String[] args) {
+//		File f = new File("c:/aaa/a1.txt");
+//		try {
+//			FileInputStream fis = new FileInputStream(f);
+//			int read = 0;
+//			while ((read = fis.read()) != -1 ) {
+//				System.out.println((char)read);
+//			}
+//			System.out.println();
+//			fis.close();
+//			
+//		} catch (Exception e ) {e.printStackTrace();}
+//		System.out.println("프로그램종료");
+		// 파일복사
+//		File f = new File("c:/aaa/a1.txt");
+//		FileInputStream fis = new FileInputStream(f);
+//		// a2.jpg로 복사
+//		FileOutputStream fos = new FileOutputStream("c:/aaa/a2.txt");
+//		
+//		while(true) {
+//			int read = fis.read();
+//			if(read == -1) break;
+//			fos.write(read);
+//		}
+//		
+//		fis.close();
+//		fos.close();
+//		
+//		System.out.println("파일 복사 완료!!");
+	}
+
+}
