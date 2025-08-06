4fb1b65368c62f37e6372c7e072fdfac01dd1068
HanaPark1 <ydjxlst@gmail.com>
2025-08-06 18:27:40 +0900

    practice: 학생성적프로그램 등수처리, 이름|합계|번호 정렬, 학생삭제

--- Ja0805/src/Studeck.java
+++ Ja0805/src/Studeck.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Studeck {
@@ -13,6 +14,7 @@ public class Studeck {
 	Student s;
 	String[] title = {"번호","이름","국어","수학","영어","합계","평균","등수"};
 	int[] score = new int[3];
+	int temp = 0;
 	
 	// 파일읽어오기 메소드
 	public void stu_FileRead(ArrayList<Student> list) throws Exception {
@@ -47,7 +49,9 @@ public class Studeck {
 		System.out.println("4. 성적삭제");
 		System.out.println("5. 등수처리");
 		System.out.println("6. 이름정렬");
-		System.out.println("7. 파일저장");
+		System.out.println("7. 합계정렬");
+		System.out.println("8. 번호정렬");
+		System.out.println("9. 파일저장");
 		System.out.println("0. 프로그램종료");
 		System.out.println("-------------------");
 		System.out.println("원하는 번호를 입력하세여>>>>>>>>>>>>>>");
@@ -81,6 +85,220 @@ public class Studeck {
 		System.out.println();
 	}//stu_ouput
 	
+	// 학생 성적 수정 메소드
+	void stu_update(ArrayList<Student> list) {
+		System.out.println();
+		System.out.println("[ 학생성적수정 ]");
+		System.out.println("수정하려는 학생 이름을 입력하세요:");
+		name = scan.next();
+		temp = 0; // 학생성검색 유무
+		
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).getName().equals(name)) {
+//				System.out.printf("%s 학생을 찾았습니다. \n", list.get(i).getName());
+				temp = 1; // 검색됨
+				no = i; // i번째 학생 정보 가져오기
+				break;
+			} 
+		} // for
+		
+		if (temp == 1 ) {
+			
+		System.out.println();
+		System.out.println("[ 수정 과목 선택 ]");
+		System.out.println("1. 국어");
+		System.out.println("2. 영어");
+		System.out.println("3. 수학");
+		System.out.println("----------------------");
+		System.out.println("원하는 번호를 입력하새요. ");
+		choice = scan.nextInt();
+		
+		switch(choice) {
+		case 1: case 2: case 3:
+			Student s = list.get(no);
+			System.out.printf("[ %s 선택 ]\n", title[choice+2]);
+			System.out.printf("현재 %s 점수:  %d\n", title[choice+2],list.get(no).getKor());
+			if (choice == 1) {
+				s.setKor(scan.nextInt());
+			} else if (choice == 2) {
+				s.setEng(scan.nextInt());
+			} else {
+				s.setMath(scan.nextInt());
+			}
+			s.setTotal(s.getKor()+s.getEng()+s.getMath());
+			s.setAvg(s.getTotal()/3.0);
+			System.out.printf("%s 점수가 수정되었습니다.\n", title[choice+2]);
+			break;
+		} //switch
+		
+		} else {
+			System.out.println("검색된 학생이 없습니다.");
+		} // if문
+		
+	}
+	
+	// 학생성적삭제 메소드
+	void stu_delete(ArrayList<Student> list) {
+		System.out.println();
+		System.out.println(" [ 학생성적삭제 ]");
+		System.out.println("삭제할 학생의 이름을 입력하세요: ");
+		name = scan.next();
+		for (int i=0; i<list.size(); i++) {
+			if(list.get(i).getName().equals(name)) {
+				temp = 1;
+				no = i;
+				break;
+			}
+		} // for
+		if(temp == 1 ) {
+			System.out.printf("%s 학생을 삭제하겠습니까? (1. 예)\n", name);
+			int choice = scan.nextInt();
+			if (choice == 1) {
+				list.remove(no);
+				Student.setCount(Student.getCount()-1);
+				System.out.printf("%s 학생을 삭제하였습니다.\n", name);
+			} else {
+				System.out.printf("%s 학생을 삭제를 취소하였습니다.\n", name);
+			}
+		} else {
+			System.out.println("검색된 학생이 없습니다.");
+		}
+	}
+	
+	public void stu_rank(ArrayList<Student> list) {
+		System.out.println("[ 등수처리 ]");
+		System.out.println("등수처리가 완료되었습니다.");
+		
+		for(int i=0; i<list.size(); i++) {
+			int count = 1;
+			for(int j=0; j<list.size(); j++) {
+				if(list.get(i).getTotal()<list.get(j).getTotal()) {
+					count++;
+				}
+			}
+			list.get(i).setRank(count);
+		}
+		
+	}
+
+	// 이름정렬 메소드
+	public void sort_name(ArrayList<Student> list) {
+		System.out.println("[ 이름정렬 ]");
+		System.out.println("1. 이름순차정렬");
+		System.out.println("2. 이름역순정렬");
+		System.out.println("0. 정렬취소");
+		System.out.println("----------------");
+		System.out.println("원하는 번호를 입력하세요>> ");
+		choice = scan.nextInt();
+		
+		switch (choice) {
+		case 1:
+			System.out.println("[ 이름순차정렬 ]");
+			list.sort(new Comparator<Student>() {
+				@Override
+				public int compare(Student o1, Student o2) {
+					// o1이 더 크면 순서변경
+					return o1.getName().compareTo(o2.getName());
+				}
+				
+			});
+			break;
+			
+		case 2:
+			System.out.println("[ 이름역순정렬 ]");
+			list.sort(new Comparator<Student>() {
+				@Override
+				public int compare(Student o1, Student o2) {
+					// o2가 더 크면 순서변경
+					return o2.getName().compareTo(o1.getName());
+				}
+				
+			});
+		case 0:
+			System.out.println("정렬을 취소합니다");
+			break;
+		}
+	}
+	
+	// 7. 합계정렬
+	public void sort_total(ArrayList<Student> list) {
+		System.out.println("[ 합계 정렬 ]");
+		System.out.println("1. 합계순차정렬");
+		System.out.println("2. 합계역순정렬");
+		System.out.println("0. 정렬취소");
+		System.out.println("----------------");
+		System.out.println("원하는 번호를 입력하세요>> ");
+		choice = scan.nextInt();
+		
+		switch (choice) {
+		case 1:
+			System.out.println("[ 합계순차정렬 ]");
+			list.sort(new Comparator<Student>() {
+				@Override
+				public int compare(Student o1, Student o2) {
+					// o1이 더 크면 순서변경
+					return o1.getTotal() - o2.getTotal();
+				}
+				
+			});
+			break;
+			
+		case 2:
+			System.out.println("[ 합계역순정렬 ]");
+			list.sort(new Comparator<Student>() {
+				@Override
+				public int compare(Student o1, Student o2) {
+					// o1이 더 크면 순서변경
+					return o2.getTotal() - o2.getTotal();
+				}
+				
+			});
+		case 0:
+			System.out.println("정렬을 취소합니다");
+			break;
+		}
+		
+	}
+	
+	//번호정렬
+	public void sort_no(ArrayList<Student> list) {
+		System.out.println("[ 합계 정렬 ]");
+		System.out.println("1. 번호순차정렬");
+		System.out.println("2. 번호역순정렬");
+		System.out.println("0. 정렬취소");
+		System.out.println("----------------");
+		System.out.println("원하는 번호를 입력하세요>> ");
+		choice = scan.nextInt();
+		
+		switch (choice) {
+		case 1:
+			System.out.println("[ 번호순차정렬 ]");
+			list.sort(new Comparator<Student>() {
+				@Override
+				public int compare(Student o1, Student o2) {
+					// o1이 더 크면 순서변경
+					return o1.getNo() - o2.getNo();
+				}
+				
+			});
+			break;
+			
+		case 2:
+			System.out.println("[ 번호역순정렬 ]");
+			list.sort(new Comparator<Student>() {
+				@Override
+				public int compare(Student o1, Student o2) {
+					// o2 더 크면 순서변경
+					return o2.getNo() - o2.getNo();
+				}
+				
+			});
+		case 0:
+			System.out.println("정렬을 취소합니다");
+			break;
+		}
+	}
+	
 	// 파일 저장
 	void stu_FileWrite(ArrayList<Student> list) throws Exception {
 		FileWriter fw = new FileWriter("/Users/hana/downloads/aaa/student.txt");
@@ -95,5 +313,7 @@ public class Studeck {
 		fw.close();
 		System.out.println("파일 저장하기 성공");
 	}//stu_FileWrite
+
+	
 
 }
--- Ja0805/src/Stumain.java
+++ Ja0805/src/Stumain.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Stumain {
 
 	public static void main(String[] args) throws Exception  {
+		Scanner scan = new Scanner(System.in);
 		ArrayList<Student> list = new ArrayList<Student>();
 		int choice;
 		int count = 0, no=0, kor=0, eng=0, math=0, total=0, rank=0; 
@@ -10,6 +13,7 @@ public class Stumain {
 		String name;
 		String[] title = {"번호","이름","국어","수학","영어","합계","평균","등수"};
 		int[] score = new int[3];
+		int temp = 0;
 		
 		Studeck studeck = new Studeck();
 		// 파일읽어오기 메소드
@@ -29,16 +33,30 @@ public class Stumain {
 				break;
 			// 성적수정
 			case 3:
+				studeck.stu_update(list);
 				break;
 			// 성적삭제
 			case 4:
+				studeck.stu_delete(list);
 				break;
+			// 등수처리
 			case 5:
+				studeck.stu_rank(list);
 				break;
+			// 이름정렬
 			case 6:
+				
+				break;
+			// 합계정렬
+			case 7:
+				studeck.sort_total(list);
+				break;
+			// 번호정렬
+			case 8:
+				studeck.sort_no(list);
 				break;
 			// 파일저장
-			case 7:
+			case 9:
 				studeck.stu_FileWrite(list);
 				break;
 			case 0:
